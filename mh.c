e9c03ce85f2f2357d51a655745ce1196708fd1df
Стив <kenethra.74@gmail.com>
2022-05-29 14:08:04 -0600

    feat: add branch and rm branch

--- mh.c
+++ mh.c
@@ -83,3 +83,91 @@ static bool link_branch(struct mh *mh, struct mh_branch *new_branch) {
   }
   return true;
 }
+
+static bool add_branch(struct mh *mh) {
+  if (mh->pool_branches.cur == mh->pool_branches.len) {
+    return false;
+  }
+
+  struct mh_branch *new_branch;
+  new_branch = ((struct mh_branch *)mh->pool_branches.buf);
+  new_branch += mh->pool_branches.cur;
+
+  new_branch->elements = ((uint8_t *)mh->pool_elements.buf);
+  new_branch->elements += mh->pool_elements.cur * mh->sizeof_element;
+
+  new_branch->parent = NULL;
+  new_branch->siblings = NULL;
+  new_branch->children = NULL;
+  new_branch->children_len = 0;
+  new_branch->element_len = 0;
+  ++(mh->pool_branches.cur);
+
+  if (link_branch(mh, new_branch) == false) {
+    return false;
+  }
+
+  mh->selected_branch = new_branch;
+  mh->selected_element = 0;
+
+  return true;
+}
+
+static bool rm_branch(struct mh *mh, struct mh_branch *selected_branch,
+                      void *ptr) {
+  struct mh_branch *parent = selected_branch->parent;
+  bool run = true;
+
+  if (selected_branch == mh->selected_branch) {
+    struct mh_branch *process = parent->children;
+
+    if (process == selected_branch) {
+      parent->children = selected_branch->siblings;
+    } else {
+      while (process->siblings != selected_branch) {
+        process = process->siblings;
+      }
+
+      process->siblings = selected_branch->siblings;
+    }
+
+    --(parent->children_len);
+    run = false;
+  }
+
+  if (mh->pool_holes.cur == mh->pool_holes.len) {
+    run = false;
+  }
+
+  struct mh_hole *new_hole;
+  new_hole = (struct mh_hole *)mh->pool_holes.buf;
+  new_hole += mh->pool_holes.cur;
+
+  uint16_t element_offset =
+      selected_branch->elements - ((uint8_t *)mh->pool_elements.buf);
+  new_hole->id = element_offset / mh->sizeof_element;
+  new_hole->len = selected_branch->element_len;
+  ++(mh->pool_holes.cur);
+
+  struct mh_branch *last_branch;
+  last_branch = (struct mh_branch *)mh->pool_branches.buf;
+  last_branch += mh->pool_branches.cur - 1;
+
+  if (selected_branch != last_branch) {
+    struct mh_branch *process = parent->children;
+
+    memcpy(selected_branch, last_branch, sizeof(struct mh_branch));
+
+    if (process == last_branch) {
+      parent->children = selected_branch;
+    } else {
+      while (process->siblings != selected_branch) {
+        process = process->siblings;
+      }
+      process->siblings = selected_branch;
+    }
+  }
+
+  --(mh->pool_branches.cur);
+  return run;
+}
