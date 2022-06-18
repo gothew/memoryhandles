636b042af81b24a47630460e6d74b8db85179565
Стив <kenethra.74@gmail.com>
2022-06-18 07:40:25 -0600

    feat: add methods set branch child and parent

--- short.c
+++ short.c
@@ -12,3 +12,29 @@ bool mh_element_prev(struct mh *mh) {
   }
   return false;
 }
+
+bool mh_branch_child(struct mh *mh) {
+  if (mh->selected_branch->children != NULL) {
+    mh->selected_branch = mh->selected_branch->children;
+
+    return true;
+  }
+  return false;
+}
+
+bool mh_element_set(struct mh *mh, uint16_t id) {
+  if (id < mh->selected_branch->element_len) {
+    mh->selected_element = id;
+    return true;
+  }
+
+  return false;
+}
+
+bool mh_branch_parent(struct mh *mh) {
+  if (mh->selected_branch->parent != NULL) {
+    mh->selected_branch = mh->selected_branch->parent;
+    return true;
+  }
+  return false;
+}
--- short.h
+++ short.h
@@ -6,5 +6,8 @@
 #include <stdint.h>
 
 bool mh_element_prev(struct mh *mh);
+bool mh_branch_parent(struct mh *mh);
+bool mh_branch_child(struct mh *mh);
+bool mh_element_set(struct mh *mh, uint16_t id);
 
 #endif
